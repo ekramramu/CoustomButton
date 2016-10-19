9655a2fed5921a05d676013672e6ea44c79d4b14
ekramramu <ekramramu@gmail.com>
2016-10-20 01:25:00 +0600

    Initial Commit

---
+++ CoustomButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoustomButton
+//
+//  Created by Ekramul hoque on 10/20/16.
+//  Copyright © 2016 Ekramul hoque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
